4c543035ca23b82b33a8adfe02ca0869fe5ca184
lalit6393 <amanengineer132@gmail.com>
2023-03-01 19:20:10 +0530

    modified create promises file

--- CreatePromise.js
+++ CreatePromise.js
@@ -1,19 +1,20 @@
-// create promise
-// how to resolve promises
-const promise2 = new Promise((resolve, reject) => {
+// how to create & resolve promises
+const promise = new Promise((resolve, reject) => {
   // resolve is a function which fulfill the promise
   resolve('Promise 1 resolved.');
 });
-// how to resolve promises
-const promise3 = new Promise((resolve, reject) => {
+
+promise.then(console.log)
+.catch(console.log);
+
+
+// how to create & reject promises
+const promise1 = new Promise((resolve, reject) => {
   // reject is a function which reject the promise
   reject('Promise 3 rejected.');
 });
 
-promise2.then(console.log)
-.catch(console.log);
-
-promise3.then(console.log)
+promise1.then(console.log)
 .catch(console.log);
 
 
